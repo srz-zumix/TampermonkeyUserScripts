a1c6f8b921fbe7eb9ed9110fd611dd375ead9837
srz_zumix <zumix.cpp@gmail.com>
2020-03-12 22:29:34 +0900

    no referrer

---
+++ READEME.md
@@ -0,0 +1,16 @@
+# TampermonkeyUserScritps
+
+## no-referrer
+
+Add to referrer meta
+
+```
+<meta name="referrer" content="no-referrer">
+<meta name="referrer" content="same-origin">
+```
+
+1. [Install](https://github.com/srz-zumix/TampermonkeyUserScripts/raw/master/no-referrer.user.js)
+1. [Open Tampermonkey Option](chrome-extension://dhdgffkkebhmkfjojejmpbldmpobfkfo/options.html)
+1. Select "no referrer"
+1. Open Settings
+1. Add url to include
---
+++ no-referrer.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name        no referrer
+// @version     0.1
+// @description Add to referrer meta ['no-referrer' and 'same-origin']
+// @author      srz_zumix
+// @namespace   https://github.com/srz-zumix/TampermonkeyUserScripts
+// @run-at      document-idle
+// @icon        https://github.githubassets.com/pinned-octocat.svg
+// @updateURL   https://github.com/srz-zumix/TampermonkeyUserScripts/raw/master/no-referrer.user.js
+// @downloadURL https://github.com/srz-zumix/TampermonkeyUserScripts/raw/master/no-referrer.user.js
+// ==/UserScript==
+(() => {
+    function init(){
+        // <meta name="referrer" content="no-referrer"> を挿入
+        // https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/Referrer-Policy
+        //
+        // no-referrer
+        //   Referer ヘッダー全体が省略されます。リクエストとともにリファラー情報が送られることはありません。
+        // same-origin
+        //   同じオリジンにはリファラーが送信されますが、オリジン間リクエストではリファラー情報が含まれません。
+        //
+        // 複数の値を指定すると、ブラウザーが対応しているもので最後に指定された値が適用されます
+
+        console.log("insert no-referrer")
+        var noreferrer_contents = ['no-referrer', 'same-origin']
+        var noreferrer = false
+        var head = $('head');
+        if( !noreferrer ) {
+            noreferrer_contents.forEach(function( value ) {
+                const meta = document.createElement("meta");
+                meta.setAttribute("name", "referrer");
+                meta.setAttribute("content", value);
+                head.appendChild(meta);
+                console.log( value );
+            });
+        }
+    }
+    function $(selector, el) {
+        return (el || document).querySelector(selector);
+    }
+
+    function $$(selector, el) {
+        return [...(el || document).querySelectorAll(selector)];
+    }
+
+    init();
+})();
